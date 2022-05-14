e034782f55d7785e46ea0d265052b63594d6f95f
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-05-14 07:38:39 +0530

    build simple filter component in react

---
+++ react-food-filtering-app/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Data from "./Data";
+import Card from "./components/Card";
+ import Buttons from "./components/Buttons";
+const App = () => {
+  const [item, setItem] = useState(Data);
+  const menuItems = [...new Set(Data.map((Val) => Val.category))];
+
+  const filterItem = (curcat) => {
+    const newItem = Data.filter((newVal) => {
+      return newVal.category === curcat;
+    });
+    setItem(newItem);
+  };
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <h1 className="col-12 text-center my-3 fw-bold">Food Filtering App</h1>
+          <Buttons
+            filterItem={filterItem}
+            setItem={setItem}
+            menuItems={menuItems}
+          />
+          <Card item={item} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
