9730f8da796e529240263b04d5ff7f2b96ea96d7
skbiswal01 <97391127+skbiswal01@users.noreply.github.com>
2022-05-17 20:38:06 +0530

    created-react-stopwatch-app

---
+++ react-stopwatch-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StopWatch from './components/StopWatch/StopWatch.js';
+   
+function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  );
+}
+  
+export default App;
